e14ee283a6df69684a7bd70f23340b95125de01d
Ilijevski <ilijevski93@gmail.com>
2014-06-20 18:55:01 +0200

    forma mrtvacka register

---
+++ js/layout.js
@@ -0,0 +1,51 @@
+(function($){
+	var initLayout = function() {
+		var hash = window.location.hash.replace('#', '');
+		var currentTab = $('ul.navigationTabs a')
+							.bind('click', showTab)
+							.filter('a[rel=' + hash + ']');
+		if (currentTab.size() == 0) {
+			currentTab = $('ul.navigationTabs a:first');
+		}
+		showTab.apply(currentTab.get(0));
+		$('.inputDate').DatePicker({
+			format:'d/m/Y',
+			date: $('#inputDate').val(),
+			current: $('#inputDate').val(),
+			starts: 1,
+			position: 'bottom',
+			onBeforeShow: function(){
+				$('#inputDate').DatePickerSetDate($('#inputDate').val(), true);
+			},
+			onChange: function(formated, dates){
+				$('#inputDate').val(formated);
+				$('#inputDate').DatePickerHide();
+			}
+		});
+		var now3 = new Date();
+		now3.addDays(-4);
+		var now4 = new Date()
+		var state = false;
+		$('#widgetField>a').bind('click', function(){
+			$('#widgetCalendar').stop().animate({height: state ? 0 : $('#widgetCalendar div.datepicker').get(0).offsetHeight}, 500);
+			state = !state;
+			return false;
+		});
+		$('#widgetCalendar div.datepicker').css('position', 'absolute');
+	};
+	
+	var showTab = function(e) {
+		var tabIndex = $('ul.navigationTabs a')
+							.removeClass('active')
+							.index(this);
+		$(this)
+			.addClass('active')
+			.blur();
+		$('div.tab')
+			.hide()
+				.eq(tabIndex)
+				.show();
+	};
+	
+	EYE.register(initLayout, 'init');
+})(jQuery)
